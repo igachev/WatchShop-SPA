94344ab4d7cf289d8dd3458951067649a7f09789
igachev <ivophoenix1135@gmail.com>
2023-06-23 14:25:16 +0300

    init database

---
+++ server/config/initDatabase.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+async function setupDatabase() {
+    mongoose.set('strictQuery',false)
+    await mongoose.connect('mongodb://127.0.0.1:27017/watchShop')
+    console.log('db connected');
+}
+
+module.exports = setupDatabase
