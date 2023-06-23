69a4ef9a6d9e8062dc3c9236a596259d19bf4968
igachev <ivophoenix1135@gmail.com>
2023-06-23 14:28:03 +0300

    start server after DB initialization

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ const express = require('express')
 const app = express()
 
 const setupDatabase = require('./config/initDatabase.js')
+
+
+setupDatabase()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`server started...`);
+    })
+})
