0aafc7167a35b7c40eed4ca063b377d24865f28b
igachev <ivophoenix1135@gmail.com>
2023-07-13 12:49:37 +0300

    functional guard to protect only  logged in user access

---
+++ client/app/src/app/guards/user-auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+export const userAuthGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const userService = inject(UserService)
+  const isLogged = userService.isLogged()
+  const isAdmin = userService.isAdmin()
+
+  if(isLogged && !isAdmin) {
+    return true
+  }
+  else {
+    return router.createUrlTree(['/login'])
+  }
+};
