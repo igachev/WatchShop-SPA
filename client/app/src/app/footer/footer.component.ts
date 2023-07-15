9882d0f6dde20961afa1e67240a673c762ee11aa
igachev <ivophoenix1135@gmail.com>
2023-07-15 23:02:55 +0300

    added email subscription functionality
    Each visitor can subscribe for our newsletter by giving his email and pressing subscribe

--- client/app/src/app/footer/footer.component.ts
+++ client/app/src/app/footer/footer.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SubscribeService } from '../services/subscribe.service';
+import { ToastService } from '../services/toast.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy {
+message!: string;
+emailSubscription!: Subscription;
+
+constructor(private subscribeService: SubscribeService,
+  private toastService: ToastService) { }
+
+sendEmail(myForm: NgForm): void {
+const {subscriptionEmail} = myForm.value;
+
+
+ this.emailSubscription = this.subscribeService.sendEmail(subscriptionEmail).subscribe({
+    next: () => {
+      myForm.reset();
+      this.message = 'Email sent';
+      this.toastService.showToast('info',this.message,true);
+    },
+    error: (err:HttpErrorResponse) => {
+      if (err.error instanceof ErrorEvent) {
+        // Client-side error occurred
+        this.message = 'An error occurred. Please try again later.';
+        this.toastService.showToast('error',this.message,true)
+      }
+      
+      else {
+        // Server-side error occurred
+        this.message = err.error.message || 'An unknown error occurred.';
+        this.toastService.showToast('error',this.message,true)
+        
+      }
+    }
+  })
+}
+
+ngOnDestroy(): void {
+  if(this.emailSubscription) {
+    this.emailSubscription.unsubscribe()
+  }
+}
 
 }
