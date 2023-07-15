0b0c73127882e8164ce4caf17c55c11923d4eaeb
igachev <ivophoenix1135@gmail.com>
2023-07-15 22:55:18 +0300

    added error handling for the subscription email on the back end

--- server/controllers/subscriptionController.js
+++ server/controllers/subscriptionController.js
@@ -3,10 +3,10 @@ const subscribeService = require('../services/subscribeService.js')
 const {getErrorMessage}= require('../utils/errorMsg.js')
 
 router.post('/', async (req,res) => {
-    const {email} = req.body;
+    const {subscriptionEmail} = req.body;
 
     try {
-        const sendEmail = await subscribeService.sendEmail(email)
+        const sendEmail = await subscribeService.sendEmail(subscriptionEmail)
         res.status(200).json(sendEmail)
     } catch (err) {
         res.status(400).json({message: getErrorMessage(err)})
--- server/services/subscribeService.js
+++ server/services/subscribeService.js
@@ -4,6 +4,10 @@ const subject = 'Welcome to WatchShop!'
 const message = 'Thanks for your subscription.Each week you will receive our newsletter on this email address.'
 
 exports.sendEmail = async (email) => {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g
+    if(!regex.test(email)) {
+        throw new Error('Invalid Email')
+    }
     let transporter = nodemailer.createTransport({
        // service: 'gmail',
         host: "smtp.abv.bg",
