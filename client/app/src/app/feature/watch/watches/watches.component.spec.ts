5af99a5b17cb73088204bbd54369c6155febd404
igachev <ivophoenix1135@gmail.com>
2023-07-28 14:24:50 +0300

    refactor tests for watches component

--- client/app/src/app/feature/watch/watches/watches.component.spec.ts
+++ client/app/src/app/feature/watch/watches/watches.component.spec.ts
@@ -16,60 +16,42 @@ import { SharedModule } from 'src/app/shared/shared.module';
 fdescribe('WatchesComponent', () => {
   let component: WatchesComponent;
   let fixture: ComponentFixture<WatchesComponent>;
-  let watchService: WatchService;
-  let userService: UserService;
+  let mockWatchService: jasmine.SpyObj<WatchService>;
+  let mockUserService: jasmine.SpyObj<UserService>;
   let el: DebugElement;
-  let mockLocalStorage: { [key: string]: string } = {};
 
   beforeEach(() => {
+
+    let mockActivatedRoute = {
+      snapshot: {
+        params: {
+          watchId: ['1','2','3','4','5']
+        }
+      }
+    }
+
+    mockWatchService = jasmine.createSpyObj('WatchService',['getAll'])
+    mockUserService = jasmine.createSpyObj('UserService',['isLogged','isAdmin'])
+
     TestBed.configureTestingModule({
       declarations: [WatchesComponent,ToastComponent, PaginatePipe],
       providers: [
-        WatchService,
-        UserService,
         PaginationService,
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              paramMap: convertToParamMap({}) // Provide an empty paramMap
-            },
-            queryParams: of({}) // Provide an empty object for queryParams
-          }
-        }
+        { provide: WatchService , useValue: mockWatchService },
+        { provide: UserService, useValue: mockUserService },
+        { provide:ActivatedRoute , useValue: mockActivatedRoute },
       ],
       imports: [HttpClientModule,NgxPaginationModule,RouterModule,SharedModule] 
     });
 
-    watchService = TestBed.inject(WatchService)
-    userService = TestBed.inject(UserService)
     fixture = TestBed.createComponent(WatchesComponent);
     component = fixture.componentInstance;
+    mockWatchService.getAll.and.returnValue(of(WATCHES))
     fixture.detectChanges();
     el = fixture.debugElement;
     
-    mockLocalStorage = {};
- spyOn(localStorage, 'getItem').and.callFake((key: string) => {
-    return mockLocalStorage[key] || null;
   });
 
-  spyOn(localStorage, 'setItem').and.callFake((key: string, value: string) => {
-    mockLocalStorage[key] = value;
-  });
-
-  spyOn(localStorage, 'removeItem').and.callFake((key: string) => {
-    delete mockLocalStorage[key];
-  });
-
-  spyOn(localStorage, 'clear').and.callFake(() => {
-    mockLocalStorage = {};
-  });
-
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 
   it('should have correct h2 text content', () => {
     let h2Element = el.query(By.css('h2'))
@@ -80,8 +62,6 @@ fdescribe('WatchesComponent', () => {
   });
 
   it('should consist of 5 or less watches because pagination limit is 5',() => {
-    component.watches = WATCHES
-    fixture.detectChanges()
     let cardElements = el.queryAll(By.css('.card'))
     expect(cardElements.length).toBeLessThanOrEqual(5)
     expect(cardElements.length).not.toBe(6)
@@ -89,21 +69,32 @@ fdescribe('WatchesComponent', () => {
   })
 
   it('guest user should see the message:You must be logged in to buy',() => {
-    component.watches = WATCHES
+   mockUserService.isLogged.and.returnValue(false)
     fixture.detectChanges()
-    const smallElements = fixture.debugElement.queryAll(By.css('small'))
-    const isLogged = userService.isLogged()
-    expect(isLogged).toBeFalse()
+    const smallElements = el.queryAll(By.css('small'))
     expect(smallElements[0].nativeElement.textContent).toBe('You must be logged in to buy')
   })
 
   it('logged in user should not see the message:You must be logged in to buy',() => {
-    component.watches = WATCHES
-    localStorage.setItem('accessToken','123')
+    mockUserService.isLogged.and.returnValue(true)
     fixture.detectChanges()
-    const smallElements = fixture.debugElement.queryAll(By.css('small'))
-    const isLogged = userService.isLogged()
-    expect(isLogged).toBeTrue()
+    const smallElements = el.queryAll(By.css('small'))
     expect(smallElements[0]).toBeUndefined()
+  })
+
+  it('the first watch card should display correct watch card details', () => {
+    const firstWatchCardImg = el.query(By.css('.card .card-img-top'))
+    expect(firstWatchCardImg.nativeElement.src).toBe('https://images.unsplash.com/photo-1533139502658-0198f920d8e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=742&q=80')
+    const firstWatchBrandAndModel = el.queryAll(By.css('.card-title'))
+    expect(firstWatchBrandAndModel[0].nativeElement.textContent).toBe('Brand: Casio')
+    expect(firstWatchBrandAndModel[1].nativeElement.textContent).toBe('Model: G-Shock')
+    const firstWatchPrice = el.queryAll(By.css('.card-text'))
+    expect(firstWatchPrice[0].nativeElement.textContent).toBe('Price: €200.00')
+  })
+
+  it('should see correct pagination (only 2 pages for 6 watches)', () => {
+    const spans = el.queryAll(By.css('span'))
+    expect(spans[4].nativeElement.textContent).toBe('1')
+    expect(spans[6].nativeElement.textContent).toBe('2')
   })
 });
