60b5d1c8b7387cb873aca9ca522113e6f31e0ae0
igachev <ivophoenix1135@gmail.com>
2023-08-02 21:40:58 +0300

    added tests to about us component

--- client/app/src/app/about-us/about-us.component.spec.ts
+++ client/app/src/app/about-us/about-us.component.spec.ts
@@ -1,21 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutUsComponent } from './about-us.component';
+import { DebugElement } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { By } from '@angular/platform-browser';
 
-describe('AboutUsComponent', () => {
+fdescribe('AboutUsComponent', () => {
   let component: AboutUsComponent;
   let fixture: ComponentFixture<AboutUsComponent>;
+  let el: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AboutUsComponent]
+      declarations: [AboutUsComponent],
+      imports: [NgbCarouselModule]
     });
     fixture = TestBed.createComponent(AboutUsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    el = fixture.debugElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have 6 images', () => {
+    const images = el.queryAll(By.css('img'))
+    expect(images.length).toBe(6)
   });
+
+  it('should use ngb-carousel', () => {
+    const ngbCarousel = el.query(By.css('ngb-carousel'))
+    expect(ngbCarousel).toBeDefined()
+  })
 });
