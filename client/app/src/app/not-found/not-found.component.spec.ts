260a27a1a1ba21012ec99d79c71b6d361cfd8dab
igachev <ivophoenix1135@gmail.com>
2023-08-02 22:03:24 +0300

    added tests to not found component

--- client/app/src/app/not-found/not-found.component.spec.ts
+++ client/app/src/app/not-found/not-found.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('NotFoundComponent', () => {
+fdescribe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
+
+  let el: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +17,22 @@ describe('NotFoundComponent', () => {
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    el = fixture.debugElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have correct h1 text content', () => {
+    const h1Element = el.query(By.css('h1'))
+    expect(h1Element.nativeElement.textContent).toBe('404')
   });
+
+  it('should have correct h2 text content', () => {
+    const h1Element = el.query(By.css('h2'))
+    expect(h1Element.nativeElement.textContent).toBe('Page Not Found')
+  });
+
+  it('should have only 1 image', () => {
+    const images = el.queryAll(By.css('img'))
+    expect(images.length).toBe(1)
+  })
 });
