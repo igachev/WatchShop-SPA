e65d8b6365027784de55ad0e2f8e8cc20438c6ae
igachev <ivophoenix1135@gmail.com>
2023-08-03 13:08:02 +0300

    fixed app architecture to comply with feature pattern

--- client/app/src/app/app.module.ts
+++ client/app/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
-import { SpinnerInterceptor } from './spinner.interceptor';
+import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CoreModule } from './core/core.module';
-import { FeatureModule } from './feature/feature.module';
 
 
 @NgModule({
@@ -31,12 +28,9 @@ import { FeatureModule } from './feature/feature.module';
     NgbCarouselModule,
     FontAwesomeModule,
     CoreModule,
-    FeatureModule
+    
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    {provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor,multi: true},
-  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/app/src/app/core/core.module.ts
+++ client/app/src/app/core/core.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { SpinnerInterceptor } from './interceptors/spinner.interceptor';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 
@@ -20,9 +22,13 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     HttpClientModule,
   ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor,multi: true},
+  ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ]
  
 })
