27d4a57b55ba87cf7b737b6c8220508c961da967
igachev <ivophoenix1135@gmail.com>
2023-08-03 13:43:57 +0300

    production commit

--- client/app/src/app/core/services/subscribe.service.ts
+++ client/app/src/app/core/services/subscribe.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IEmail } from '../../shared/interfaces/IEmail';
-import { environment } from 'src/environments/environment.development';
-//import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
